1d1102337cbf89eed69c62e0b58929e6f27953bb
debbie <drosenfeld87@gmail.com>
2018-03-26 18:15:06 -0400

    step 7 count total is working

--- ajax.js
+++ ajax.js
@@ -3,26 +3,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var button1 = document.getElementById("button1")
   button1.addEventListener('click', function(e) {
-   $.ajax ({
+   var request = $.ajax ({
     url: 'http://first-ajax-api.herokuapp.com/',
     method: 'GET',
     data: {},
-    dataType: 'json'
+    dataType: 'text'
   });
 });
     var button2 = document.getElementById("button2")
     button2.addEventListener('click', function() {
 
       var request = $.ajax ({
-      url: 'http://first-ajax-api.herokuapp.com/ping',
+      url: 'http://first-ajax-api.herokuapp.com/pong',
       method: 'GET',
       data: {},
-      dataType: 'json'
-    }).done(function (responseData) {
+      dataType: 'text'
+    }).done(function () {
       console.log("request succeeded: " + request.responseData);
     }).fail(function() {
       console.log("Request Failed! Sorry, will try harder next time");
-
+    }).always(function () {
+      console.log("Hey the request finished!");
     });
     });
+    var button3 = document.getElementById("button3")
+    button3.addEventListener('click', function(e) {
+     var request = $.ajax ({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    }).done(function (responseData) {
+      console.log(responseData);
+    });
+  });
   });
