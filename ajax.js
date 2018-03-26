7acc3575edd3874834f784b17f36ace70b9a7deb
debbie <drosenfeld87@gmail.com>
2018-03-26 17:20:04 -0400

    failure notice in console log is working

--- ajax.js
+++ ajax.js
@@ -1,15 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var button1 = document.querySelector('click1');
-  button1.addEventListener('click', function() {
-    window.alert("Button1 has been clicked");
-  })
 
-  $.ajax({
-  url: 'http://first-ajax-api.herokuapp.com/ping',
-  method: 'GET',
-  data: "",
-  dataType: 'html'
+  var button1 = document.getElementById("button1")
+  button1.addEventListener('click', function(e) {
+   $.ajax ({
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    data: {},
+    dataType: 'json'
+  });
 });
+    var button2 = document.getElementById("button2")
+    button2.addEventListener('click', function() {
 
-});
+      var request = $.ajax ({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      data: {},
+      dataType: 'json'
+    }).done(function (responseData) {
+      console.log("request succeeded: " + request.responseData);
+    }).fail(function() {
+      console.log("Request Failed! Sorry, will try harder next time");
+
+    });
+    });
+  });
