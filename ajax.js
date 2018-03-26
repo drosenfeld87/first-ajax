527e431260550e1e5cb7947faefb823a9f085980
debbie <drosenfeld87@gmail.com>
2018-03-26 15:51:03 -0400

    added button1

--- ajax.js
+++ ajax.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var request = $.ajax({
-  url: 'http://first-ajax-api.herokuapp.com/',
+  var button1 = document.querySelector('click1');
+  button1.addEventListener('click', function() {
+    window.alert("Button1 has been clicked");
+  })
+
+  $.ajax({
+  url: 'http://first-ajax-api.herokuapp.com/ping',
   method: 'GET',
-  data: {
-  },
+  data: "",
   dataType: 'html'
 });
 
